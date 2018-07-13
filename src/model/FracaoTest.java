9670d26f159ed5d2a2498dc83e6e45fcd916dc0f
Jesus <jesusadadfilho@gmail.com>
2018-07-13 16:00:53 -0300

    testes

--- src/model/FracaoTest.java
+++ src/model/FracaoTest.java
@@ -8,12 +8,14 @@ class FracaoTest {
 
     @Test
     void testConstruror(){
+        Fracao fracao = new Fracao(4,0);
+        System.out.println(fracao.valorReal());
     }
 
     @Test
     void resultado() {
         Fracao fracao = new Fracao(6,3);
-        assertEquals(2.0,fracao.resultado());
+        assertEquals(2.0,fracao.valorReal());
     }
 
 
@@ -27,13 +29,22 @@ class FracaoTest {
 
     @Test
     void subtracao() {
+        Fracao fracao1 = new Fracao(9,3);
+        Fracao fracao = new Fracao(6,3);
+        assertEquals(new Fracao(3,3).valorReal(),fracao1.subtracao(fracao).valorReal());
     }
 
     @Test
     void multiplicacao() {
+        Fracao fracao1 = new Fracao(9,3);
+        Fracao fracao = new Fracao(6,3);
+        assertEquals(new Fracao(54,9).valorReal(),fracao.multiplicacao(fracao1).valorReal());
     }
 
     @Test
     void divisao() {
+        Fracao fracao1 = new Fracao(5,9);
+        Fracao fracao = new Fracao(8,3);
+        assertEquals(new Fracao(72,15).valorReal(),fracao.soma(fracao1).valorReal());
     }
 }
